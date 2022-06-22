ad885effff76499fdb584566115cafb8054c84c8
Thanh Loi <thanhloi030301@gmail.com>
2022-06-22 20:26:03 +0700

    get category

--- src/main/java/com/nhlshop/controller/Category.java
+++ src/main/java/com/nhlshop/controller/Category.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,18 @@ public class Category {
         Collection<CategoryEntity> listCategory = categoryService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(
                 new ResponseObject("OK", "Get categories successfully", listCategory));
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<ResponseObject> getById(@PathVariable Long id) {
+        CategoryEntity category = new CategoryEntity();
+        try {
+            category = categoryService.findById(id).get();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ResponseObject("FAILED", "Cannot get category with id: " + id, ""));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("OK", "Get categories successfully", category));
     }
 }
