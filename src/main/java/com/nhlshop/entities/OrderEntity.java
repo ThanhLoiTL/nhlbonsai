85f4fcc50ddb6c5ddbdfc9d7987a6939d2e4151a
Thanh Loi <thanhloi030301@gmail.com>
2022-06-24 09:54:21 +0700

    order

--- src/main/java/com/nhlshop/entities/OrderEntity.java
+++ src/main/java/com/nhlshop/entities/OrderEntity.java
@@ -1,7 +1,9 @@
 package com.nhlshop.entities;
 
+import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -59,6 +62,9 @@ public class OrderEntity {
     @ManyToOne(optional = true)
     @JoinColumn(name = "shipper_id")
     private UserEntity shipper;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Collection<OrderDetailEntity> orderDetails;
 
     public Long getId() {
         return id;
